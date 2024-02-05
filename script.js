8f984d9ec3512cb4df029e0bf41f85660d046013
Samuel <samuferfort@gmail.com>
2024-02-05 22:40:14 +0100

    color functionality and improved layout

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const container = document.querySelector(".container");
 const rows = document.getElementsByClassName("row");
 const columns = document.getElementsByClassName("column");
+const resetColor = document.querySelector(".reset");
+const randomCOlor = document.querySelector(".random")
+
+
 
 
 function createRow(rowNumber) {
     for (let i = 0; i < rowNumber; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
+        
         container.appendChild(row);
     }
-
 }
 
 function createColumn(columnNumber) {
@@ -17,6 +21,7 @@ function createColumn(columnNumber) {
         for (let j = 0; j < columnNumber; j++) {
             let newColumn = document.createElement("div");
             newColumn.classList.add("column");
+            newColumn.addEventListener("mouseenter", () => newColumn.style.backgroundColor = "red")
             rows[i].appendChild(newColumn);
         }
     }
